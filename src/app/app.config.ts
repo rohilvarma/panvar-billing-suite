45bfea7562f4fb823471c61239aad3c47b7987a4
rohilvarma <rohilvarma96@gmail.com>
2024-12-13 01:52:41 +0530

    Init commit.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { Noir } from '../../noir-preset';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: '.dark'
+        }
+      },
+    }),
+  ],
 };
